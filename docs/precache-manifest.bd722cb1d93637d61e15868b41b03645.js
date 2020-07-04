fc58b3cfeb548cce1f372b2488100a6cc0237dc5
Franco Lucchini <franqo200@gmail.com>
2020-07-04 10:43:12 -0300

    docs added

---
+++ docs/precache-manifest.bd722cb1d93637d61e15868b41b03645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bbd3c9ecfeaf986bb2831f90c1fe86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04a47950c7f333c589d6",
+    "url": "/static/css/main.afbb53ce.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a47950c7f333c589d6",
+    "url": "/static/js/main.7cbaa950.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
